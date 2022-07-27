f6ef51fdf0e12c17adf018c7ce14b2458c0709cf
Devesh-62 <54881249+Devesh-62@users.noreply.github.com>
2022-07-27 23:03:09 +0530

    Update Palindrone.java

--- Arrays/Easy/Palindrone.java
+++ Arrays/Easy/Palindrone.java
@@ -40,3 +40,18 @@ for(int i =0;i<n;i++)
    
 }
            }
+
+or
+
+   if (x < 0) {
+            return false;
+        }
+        // Store the number in a variable
+        int number = x;
+        // This will store the reverse of the number
+        int reverse = 0;
+        while (number > 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        return x == reverse;
